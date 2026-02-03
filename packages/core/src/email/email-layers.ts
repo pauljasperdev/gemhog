c7893b70a90c117044973920b60a5e98c4a5a86e
Paul Jasper <mail@pauljasper.dev>
2026-02-03 12:59:53 +0100

    refactor: remove env dependecy, read from proces.env via Config

--- packages/core/src/email/email-layers.ts
+++ packages/core/src/email/email-layers.ts
@@ -1,25 +1,12 @@
-import { ServerEnvLive, ServerEnvService } from "@gemhog/env/server";
-import { Effect, Layer } from "effect";
+import { Layer } from "effect";
 import { DatabaseLive } from "../drizzle/index";
-import { EmailServiceConsole, EmailServiceLive } from "./email.service";
+import { EmailServiceLive } from "./email.service";
 import { SubscriberServiceLive } from "./subscriber.service";
 
-const DEV_PLACEHOLDER_KEY = "re_local_dev_placeholder";
-
-const EmailServiceResolved = Layer.unwrapEffect(
-  Effect.gen(function* () {
-    const { RESEND_API_KEY } = yield* ServerEnvService;
-    return RESEND_API_KEY === DEV_PLACEHOLDER_KEY
-      ? EmailServiceConsole
-      : EmailServiceLive;
-  }),
-);
-
 export const EmailLayers = Layer.mergeAll(
-  EmailServiceResolved,
+  EmailServiceLive,
   SubscriberServiceLive.pipe(
     Layer.provide(DatabaseLive),
-    Layer.provide(EmailServiceResolved),
+    Layer.provide(EmailServiceLive),
   ),
-  ServerEnvLive,
-).pipe(Layer.provide(ServerEnvLive));
+);
