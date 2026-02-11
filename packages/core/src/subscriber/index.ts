b2c2006821b74a3b9456b9b13d24942bbfa3b1f2
Paul Jasper <mail@pauljasper.dev>
2026-02-11 20:33:54 +0100

    refactor: parameterize TracingLive, rename subscriber.layers.ts, simplify SubscriberLayers

--- packages/core/src/subscriber/index.ts
+++ packages/core/src/subscriber/index.ts
@@ -2,10 +2,10 @@ export {
   InvalidTokenError,
   SubscriberNotFoundError,
 } from "./subscriber.errors";
+export { SubscriberLayers } from "./subscriber.layers";
 export { MockSubscriberService } from "./subscriber.mock";
 export { SubscriberService, SubscriberServiceLive } from "./subscriber.service";
 export type { Subscriber } from "./subscriber.sql";
 export * as subscriberSchema from "./subscriber.sql";
-export { SubscriberLayers } from "./subscriber-layers";
 export type { TokenPayload } from "./token";
 export { createToken, verifyToken } from "./token";
--- packages/telemetry/src/index.ts
+++ packages/telemetry/src/index.ts
@@ -9,39 +9,40 @@ import { Config, Effect, Layer } from "effect";
 import { pipe } from "effect/Function";
 import * as Option from "effect/Option";
 
-export const TracingLive = Layer.unwrapEffect(
-  Effect.gen(function* () {
-    const otlpEndpoint = yield* pipe(
-      Config.string("OTEL_EXPORTER_OTLP_TRACES_ENDPOINT"),
-      Config.option,
-    );
-    const sentryDsn = yield* pipe(Config.string("SENTRY_DSN"), Config.option);
+export const makeTracingLive = (serviceName: string) =>
+  Layer.unwrapEffect(
+    Effect.gen(function* () {
+      const otlpEndpoint = yield* pipe(
+        Config.string("OTEL_EXPORTER_OTLP_TRACES_ENDPOINT"),
+        Config.option,
+      );
+      const sentryDsn = yield* pipe(Config.string("SENTRY_DSN"), Config.option);
 
-    const resourceLayer = Resource.layer({
-      serviceName: "gemhog-core",
-    });
+      const resourceLayer = Resource.layer({
+        serviceName,
+      });
 
-    if (Option.isNone(otlpEndpoint) || Option.isNone(sentryDsn)) {
-      return Tracer.layerGlobal.pipe(Layer.provide(resourceLayer));
-    }
+      if (Option.isNone(otlpEndpoint) || Option.isNone(sentryDsn)) {
+        return Tracer.layerGlobal.pipe(Layer.provide(resourceLayer));
+      }
 
-    OtelApi.propagation.setGlobalPropagator(new W3CTraceContextPropagator());
+      OtelApi.propagation.setGlobalPropagator(new W3CTraceContextPropagator());
 
-    const spanProcessor = new BatchSpanProcessor(
-      new OTLPTraceExporter({
-        url: otlpEndpoint.value,
-        headers: {
-          "x-sentry-dsn": sentryDsn.value,
-        },
-      }),
-    );
+      const spanProcessor = new BatchSpanProcessor(
+        new OTLPTraceExporter({
+          url: otlpEndpoint.value,
+          headers: {
+            "x-sentry-dsn": sentryDsn.value,
+          },
+        }),
+      );
 
-    const tracerProviderLayer = NodeSdk.layerTracerProvider(spanProcessor);
-    const tracerLayer = Tracer.layer.pipe(
-      Layer.provide(tracerProviderLayer),
-      Layer.provide(resourceLayer),
-    );
+      const tracerProviderLayer = NodeSdk.layerTracerProvider(spanProcessor);
+      const tracerLayer = Tracer.layer.pipe(
+        Layer.provide(tracerProviderLayer),
+        Layer.provide(resourceLayer),
+      );
 
-    return tracerLayer;
-  }),
-);
+      return tracerLayer;
+    }),
+  );
