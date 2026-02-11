0acdbbfadc49d5f73be568d86d0472fa67c01be5
Paul Jasper <mail@pauljasper.dev>
2026-02-11 20:33:54 +0100

    refactor: parameterize TracingLive, rename subscriber.layers.ts, simplify SubscriberLayers

--- packages/core/src/subscriber/subscriber.layers.ts
+++ packages/core/src/subscriber/subscriber.layers.ts
@@ -1,5 +1,5 @@
 import { EmailServiceConsole, EmailServiceLive } from "@gemhog/email";
-import { TracingLive } from "@gemhog/telemetry";
+import { makeTracingLive } from "@gemhog/telemetry";
 import { Layer } from "effect";
 import { DatabaseLive } from "../drizzle/index";
 import { SubscriberServiceLive } from "./subscriber.service";
@@ -7,11 +7,8 @@ import { SubscriberServiceLive } from "./subscriber.service";
 const isLocal = process.env.LOCAL_ENV === "1";
 const EmailServiceLayer = isLocal ? EmailServiceConsole : EmailServiceLive;
 
-// const EmailServiceLayer = EmailServiceLive;
-
 export const SubscriberLayers = Layer.mergeAll(
-  TracingLive,
-  EmailServiceLayer,
+  makeTracingLive("gemhog-core"),
   SubscriberServiceLive.pipe(
     Layer.provide(DatabaseLive),
     Layer.provide(EmailServiceLayer),
