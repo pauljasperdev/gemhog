824ebe33750a108191b19000381486dddc227371
Paul Jasper <mail@pauljasper.dev>
2026-02-11 15:15:42 +0100

    refactor(telemetry): extract @gemhog/telemetry package with TracingLive
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-opencode)

---
+++ packages/telemetry/src/index.ts
@@ -0,0 +1,47 @@
+import * as NodeSdk from "@effect/opentelemetry/NodeSdk";
+import * as Resource from "@effect/opentelemetry/Resource";
+import * as Tracer from "@effect/opentelemetry/Tracer";
+import * as OtelApi from "@opentelemetry/api";
+import { W3CTraceContextPropagator } from "@opentelemetry/core";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
+import { Config, Effect, Layer } from "effect";
+import { pipe } from "effect/Function";
+import * as Option from "effect/Option";
+
+export const TracingLive = Layer.unwrapEffect(
+  Effect.gen(function* () {
+    const otlpEndpoint = yield* pipe(
+      Config.string("OTEL_EXPORTER_OTLP_TRACES_ENDPOINT"),
+      Config.option,
+    );
+    const sentryDsn = yield* pipe(Config.string("SENTRY_DSN"), Config.option);
+
+    const resourceLayer = Resource.layer({
+      serviceName: "gemhog-core",
+    });
+
+    if (Option.isNone(otlpEndpoint) || Option.isNone(sentryDsn)) {
+      return Tracer.layerGlobal.pipe(Layer.provide(resourceLayer));
+    }
+
+    OtelApi.propagation.setGlobalPropagator(new W3CTraceContextPropagator());
+
+    const spanProcessor = new BatchSpanProcessor(
+      new OTLPTraceExporter({
+        url: otlpEndpoint.value,
+        headers: {
+          "x-sentry-dsn": sentryDsn.value,
+        },
+      }),
+    );
+
+    const tracerProviderLayer = NodeSdk.layerTracerProvider(spanProcessor);
+    const tracerLayer = Tracer.layer.pipe(
+      Layer.provide(tracerProviderLayer),
+      Layer.provide(resourceLayer),
+    );
+
+    return tracerLayer;
+  }),
+);
