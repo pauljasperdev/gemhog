1bf85f3a300becfe215d51850797cd0ff2c2910a
Paul Jasper <mail@pauljasper.dev>
2026-03-01 13:25:14 +0100

    feat(auth): convert sendOtpEmail to Effect.fn

--- packages/auth/src/send-otp.ts
+++ packages/auth/src/send-otp.ts
@@ -1,10 +1,13 @@
 import { EmailService, EmailServiceLayer, signInOtpEmail } from "@gemhog/email";
 import * as Effect from "effect";
+import { annotateCurrentSpan } from "effect/Effect";
 
-export function sendOtpEmail(to: string, otp: string) {
-  return Effect.Effect.gen(function* () {
+export const sendOtpEmail = Effect.Effect.fn("auth.sendOtp")(
+  function* (to: string, otp: string) {
+    yield* annotateCurrentSpan("email.to", to);
     const content = yield* Effect.Effect.promise(() => signInOtpEmail({ otp }));
     const emailService = yield* EmailService;
     yield* emailService.send(to, content);
-  }).pipe(Effect.Effect.provide(EmailServiceLayer));
-}
+  },
+  (effect) => effect.pipe(Effect.Effect.provide(EmailServiceLayer)),
+);
